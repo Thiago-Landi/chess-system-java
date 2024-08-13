a063e4040ef20a38e0b008bf3c52d03dd945b50e
Thiago Landi <thiagolandi0902@gmail.com>
2024-08-13 13:55:44 -0300

    Começando a implementar Tabuleiro e peça

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,14 +1,14 @@
 package application;
 
 import boardgame.Posicao;
+import boardgame.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
 
-		Posicao posicao = new Posicao(5, 6);
-		System.out.println(posicao);
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 		
 	}
 
