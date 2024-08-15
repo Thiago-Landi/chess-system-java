a1ffa9eaf4540ca805a7fa86a5c06e36852fa40a
Thiago Landi <thiagolandi0902@gmail.com>
2024-08-15 09:31:52 -0300

    TabuleiroException e programa defensivo

--- src/boardgame/Tabuleiro.java
+++ src/boardgame/Tabuleiro.java
@@ -7,6 +7,10 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 	
 	public Tabuleiro(int linhas, int colunas) {
+		if(linhas < 1 || colunas < 1) {
+			throw new TabuleiroException("Erro ao criar o tabuleiro: o tabuleiro tem que ter pelo menos uma linha e uma coluna");
+		}
+		
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -16,29 +20,50 @@ public class Tabuleiro {
 		return linhas;
 	}
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
+	
 	public int getColunas() {
 		return colunas;
 	}
 
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-	
+		
 	public Peca peca(int linha, int coluna) {
+		if(!posicaoExistent(linha, coluna)) {// o ! siginifica não
+			throw new TabuleiroException("Não ha essa posição no tabuleiro");
+		}
+		
 		return pecas[linha][coluna];
 	}
 	
 	public Peca peca(Posicao posicao) {
+		if(!posicaoExistent(posicao)) {// o ! siginifica não
+			throw new TabuleiroException("Não ha essa posição no tabuleiro");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 	
 	public void LugarDaPeca(Peca peca, Posicao posicao) {
+		if(possuiAPeca(posicao)) {
+			throw new TabuleiroException("Já existe peça na posição "+ posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
 	}
+	
+	private boolean posicaoExistent(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas; // linhas e colunas sao a da classe tabuleiro e o sem o S vai ser dado na hora
+	}
+	
+	public boolean posicaoExistent(Posicao posicao) {
+		return posicaoExistent(posicao.getLinha(), posicao.getColuna());
+	}
+	
+	public boolean possuiAPeca(Posicao posicao) {
+		if(!posicaoExistent(posicao)) {// o ! siginifica não
+			throw new TabuleiroException("Não ha essa posição no tabuleiro");
+		}
+		
+		return peca(posicao) != null;// o peca(posicao)é o metodo que esta no tabuleiro na linha 35
+	}
+	
 	
 }
